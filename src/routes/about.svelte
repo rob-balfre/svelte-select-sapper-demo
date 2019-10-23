7a7a368ed57fafa4e3b8cf47f869362025be41dc
Rob Balfre <robert.balfre@travelctm.com.au>
2019-10-24 08:58:47 +1100

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Select from 'svelte-select';
+
+	let items = ['one', 'two', 'three'];
+	let selectedValue = undefined;
+	
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<Select {items} bind:selectedValue></Select>
